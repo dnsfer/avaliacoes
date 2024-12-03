6e5c5899dc7f3c57a25d5a02f0aafae744d71f91
dnsfer <dnunesferreira.fr@gmail.com>
2024-12-03 06:58:30 -0300

    avaliações

---
+++ src/javascript/script.js
@@ -0,0 +1,29 @@
+var swiper = new Swiper(".swiper", {
+  loop: true,
+  grabCursor: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true, //Deixa as bolinhas clicáveis
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 18,
+    },
+    // when window width is >= 480px
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 18,
+    },
+    // when window width is >= 640px
+    1188: {
+      slidesPerView: 3,
+      spaceBetween: 24,
+    },
+  },
+});
